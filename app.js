4805a3ddbb4c8ded28f5058af7a09984cd7f91e0
Vescik <46301368+Vescik@users.noreply.github.com>
2020-10-31 17:15:49 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ const button = document.querySelector('.button');
 const displaySize = {width:camera.width,height:camera.height};
 
 
+const emotions = {
+	'neutral': 'Neutralny 😐',
+	'surprised': 'Zaskoczony 😮',
+	'disgusted': 'Zniesmaczony',
+	'fearful': 'Wystraszony 😨',
+	'sad': 'Smutny 🙁',
+	'angry': 'Zły 😠',
+	'happy': 'Wesoły 😃',
+}
+
+
 function startVideo(){
     navigator.getUserMedia({video: true},
         (stream) => {
@@ -13,4 +24,23 @@ function startVideo(){
         )
 }
 
-button.addEventListener('click', startVideo)
+startVideo();
+``
+Promise.all([
+	faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+	faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+]).then(startVideo)
+
+camera.addEventListener('play',detectFace)
+
+async function detectFace(){
+    const options = new faceapi.TinyFaceDetectorOptions();
+
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(camera,options).withFaceExpressions();
+
+        if(detections[0]){
+            console.log(detections[0])
+        }
+    },100)
+}
